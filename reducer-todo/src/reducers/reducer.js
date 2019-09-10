70f8dbfdf94593001c60c0e9e3daaed9464a1df2
Sara Alkhamri <s.alkhamri@gmail.com>
2019-09-10 13:23:05 -0700

    Created reducer and initial state object in reducer.js

---
+++ reducer-todo/src/reducers/reducer.js
@@ -0,0 +1,22 @@
+export const initialState = {
+    todo: [
+        {
+            item: 'Learn Reducers',
+            completed: false,
+            id: 801
+        },
+
+        {
+            item: 'Review Mutation',
+            completed: false,
+            id: 802
+        }
+    ]
+};
+
+export function todoReducer(state, action) {
+    return {
+        ...state,
+        
+    }
+}
